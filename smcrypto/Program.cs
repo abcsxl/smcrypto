e8d5ebda9afa7bf2a5653dffda3fa47b7acc30b5
SIM <abcsxl@msn.com>
2017-08-21 17:05:17 +0800

    添加项目文件。

---
+++ smcrypto/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace smcrypto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SM4Utils sm4 = new SM4Utils();
+            sm4.secretKey = "JeF8U9wHFOMfs2Y8";
+            sm4.hexString = false;
+
+            Console.WriteLine("ECB模式");
+            Console.WriteLine("密文: " + sm4.Encrypt_ECB("11223344556677881122334455667788".HexToByteArray(), "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF".HexToByteArray()).ByteArrayToHex());
+            Console.WriteLine("");
+
+            Console.ReadLine();
+        }
+    }
+}
